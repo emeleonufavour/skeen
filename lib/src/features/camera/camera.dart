26458fd32a7b59e238c368da9a608220cabaf6c5
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-10-05 21:29:03 +0100

    refactor(camera): Create Camera barrel file

---
+++ lib/src/features/camera/camera.dart
@@ -0,0 +1,4 @@
+export '../chat_bot/ui/views/chat_bot_view.dart';
+export '../scan_product/presentation/notifier/scan_product_notifier.dart';
+export './presentation/notifier/camera_ctr_notifier.dart';
+export './presentation/painter/camera_background_overlay.dart';
